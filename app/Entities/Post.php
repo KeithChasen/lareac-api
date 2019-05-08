36bcdcd2d99789a12528e477bbe30b81c8044321
keithchasen <keithchasen89@gmail.com>
2019-05-08 18:17:29 +0300

    basic Post doctrine entity

---
+++ app/Entities/Post.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="posts")
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $body;
+
+    public function __constructor($title, $body)
+    {
+        $this->title = $title;
+        $this->body = $body;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+    public function getBody()
+    {
+        return $this->title;
+    }
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+}
